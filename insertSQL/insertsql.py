d56065353fb564cf0bbc0374d9a606faf338c69e
wengfuyi <214952026@qq.com>
2018-02-08 15:27:45 +0800

    优化 sql 语句通过数据库索引大幅度提升查询效率

--- insertSQL/insertsql.py
+++ insertSQL/insertsql.py
@@ -2,6 +2,10 @@
 import mysql.connector
 
 
+def login_sql():
+    conn = mysql.connector.connect(user='root', password='admin', database='test')
+    return conn
+
 def ip2int(ip):
     try:
         # % X表示输出16进制形式，使用A～F的大写字符，比如15就输出F
@@ -29,23 +33,15 @@ def int2ip(n):
     # import socket, struct
     # return socket.inet_ntoa(struct.pack("!I", n))
 
-conn = mysql.connector.connect(user='root', password='admin', database='test')
-
-
-
-if __name__ == '__main__':
-    # print(ip2int('127.0.0.1'))
-    # print("%02X" % int(126))
-    # print(int('0', 16))
-    # print(int2ip(ip2int('127.0.0.1')))
-
-    #指定编码格式
+def insert_row():
+    conn = mysql.connector.connect(user='root', password='admin', database='test')
+    # 指定编码格式
     with open('./ipdata.csv', 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
     nl_p_list = []
     for l in lines:
-        #指定切割次数，并将数据中的空格删除
+        # 指定切割次数，并将数据中的空格删除
         ls = l.strip().split(',', 4)
         # print(l)
         # print(*ls)
@@ -54,15 +50,60 @@ if __name__ == '__main__':
         nl = [c1, c2, c3, c4, c5]
         nl_p_list.append(nl)
         # print(nl)
-    #连接数据数据库游标
+    # 连接数据数据库游标
     cursor = conn.cursor()
+    # ret = cursor.executemany('insert into ipdata (id, startip, endip, country, carrier) values (%s, %s, %s, %s, %s)',
+    #                          nl_p_list)
 
-    for i in range(int(len(nl_p_list)/1000 +1)):
-        tmp_nl_p_list = nl_p_list[i*1000: (i+1)*1000]
+    for i in range(int(len(nl_p_list) / 1000 + 1)):
+        tmp_nl_p_list = nl_p_list[i * 1000: (i + 1) * 1000]
         # print(tmp_nl_p_list[i][0] + ': ' + tmp_nl_p_list[i][4])
         # 批量插入表中
-        ret = cursor.executemany('insert into ipdata (id, startip, endip, country, carrier) values (%s, %s, %s, %s, %s)',
-                       tmp_nl_p_list)
+        ret = cursor.executemany(
+            'insert into ipdata (id, startip, endip, country, carrier) values (%s, %s, %s, %s, %s)',
+            tmp_nl_p_list)
     # 提交事务;关闭数据库连接
     conn.commit()
     conn.close()
+
+
+
+
+if __name__ == '__main__':
+    # print(ip2int('127.0.0.1'))
+    # print("%02X" % int(126))
+    # print(int('0', 16))
+    # print(int2ip(ip2int('127.0.0.1')))
+    with open('./ipdata.csv', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    nl_p_list = []
+    for l in lines:
+        ls = l.strip().split(',', 4)
+        c1, c2, c3, c4, c5 = ls[0], ip2int(ls[1]), ip2int(ls[2]), ls[3], ls[4]
+        nl = [c1, c2, c3, c4, c5]
+        nl_p_list.append(nl)
+
+    import random
+    import time
+
+    ip_list = map(lambda x: x[1], random.sample(nl_p_list, 100))
+    conn = login_sql()
+    cursor = conn.cursor(buffered=True)
+    # cursor.execute('SELECT * FROM ipdata WHERE 1780997668 BETWEEN startip AND endip')
+    t0 = time.time()
+    for i in ip_list:
+        cursor.execute('SELECT * FROM ipdata WHERE %s >=  startip ORDER BY startip DESC LIMIT 1' %i)
+        result = cursor.fetchall()
+    t1 = time.time()
+    # t2 = time.time()
+    for i in ip_list:
+        cursor.execute('SELECT * FROM ipdata WHERE %s BETWEEN startip AND endip' %i)
+        result = cursor.fetchall()
+        # print(result)
+    t3 = time.time()
+    print(t1 - t0)
+    print(t3 - t1)
+
+
+
